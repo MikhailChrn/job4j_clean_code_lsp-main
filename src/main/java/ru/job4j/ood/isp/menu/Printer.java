5b682b38df4c2d196b14e1cff987481023713154
MikhailChrn <mikhail.cherneyev@yandex.ru>
2024-06-16 20:22:56 +0400

    1. Создать меню. [#4748] (Добавлена реализация Printer)

--- src/main/java/ru/job4j/ood/isp/menu/Printer.java
+++ src/main/java/ru/job4j/ood/isp/menu/Printer.java
@@ -2,8 +2,28 @@ package ru.job4j.ood.isp.menu;
 
 public class Printer implements MenuPrinter {
 
+    static final String INDENTATION = "----";
+
+    private final Output out;
+
+    public Printer(Output out) {
+        this.out = out;
+    }
+
     @Override
     public void print(Menu menu) {
-        /*  добавьте реализацию*/
+        menu.forEach(menuItemInfo ->
+                out.println(getIndentation(menuItemInfo)
+                            + menuItemInfo.getNumber()
+                            + menuItemInfo.getName()));
+    }
+
+    private String getIndentation(Menu.MenuItemInfo menuItemInfo) {
+        StringBuilder sb = new StringBuilder();
+        int numb = (int) menuItemInfo.getNumber().chars()
+                .filter(ch -> ch == '.')
+                .count();
+        sb.append(INDENTATION.repeat(Math.max(0, numb - 1)));
+        return sb.toString();
     }
 }
---
+++ src/test/java/ru/job4j/ood/isp/menu/PrinterTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.ood.isp.menu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PrinterTest {
+
+    static final ActionDelegate STUB_ACTION = System.out::println;
+
+    static final String LN = System.lineSeparator();
+
+    private final Output out = new StubOutput();
+
+    private final Menu menu = new SimpleMenu();
+
+    {
+        this.menu.add(Menu.ROOT, "Сходить в магазин", STUB_ACTION);
+        this.menu.add(Menu.ROOT, "Покормить собаку", STUB_ACTION);
+        this.menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
+        this.menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
+        this.menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
+    }
+
+    @Test
+    public void whenPrintedCurrentMenu() {
+        MenuPrinter printer = new Printer(out);
+        printer.print(menu);
+        assertThat(out.toString()).isEqualTo(
+                "1.Сходить в магазин" + LN
+                        + "----1.1.Купить продукты" + LN
+                        + "--------1.1.1.Купить хлеб" + LN
+                        + "--------1.1.2.Купить молоко" + LN
+                        + "2.Покормить собаку" + LN
+        );
+    }
+
+}
