687876828fb66eb57e4b6e6fbbbb12eab24e0218
MikhailChrn <mikhail.cherneyev@yandex.ru>
2024-06-16 13:57:31 +0400

    1. Создать меню. [#4748] (Добаввлена реализация SimpleMenu)

--- src/main/java/ru/job4j/ood/isp/menu/SimpleMenu.java
+++ src/main/java/ru/job4j/ood/isp/menu/SimpleMenu.java
@@ -1,6 +1,7 @@
 package ru.job4j.ood.isp.menu;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class SimpleMenu implements Menu {
 
@@ -8,25 +9,66 @@ public class SimpleMenu implements Menu {
 
     @Override
     public boolean add(String parentName, String childName, ActionDelegate actionDelegate) {
-   /*  добавьте реализацию*/
-        return  false;
+          if (findItem(childName).isPresent()) {
+            return false;
+        }
+        if (Objects.equals(Menu.ROOT, parentName)) {
+            rootElements.add(new SimpleMenuItem(childName, actionDelegate));
+        } else {
+            findItem(parentName).get().menuItem
+                    .getChildren()
+                    .add(new SimpleMenuItem(childName, actionDelegate));
+        }
+        return true;
     }
 
     @Override
     public Optional<MenuItemInfo> select(String itemName) {
-        /*  добавьте реализацию*/
-        return null;
+        Optional<MenuItemInfo> menuItemInfo = Optional.empty();
+        Optional<ItemInfo> tmp = findItem(itemName);
+        if (tmp.isPresent()) {
+            menuItemInfo = Optional.of(new MenuItemInfo(tmp.get().menuItem.getName(),
+                    tmp.get().menuItem.getChildren().stream()
+                            .map(MenuItem::getName)
+                            .collect(Collectors.toList()),
+                    tmp.get().menuItem.getActionDelegate(),
+                    tmp.get().number));
+        }
+        return menuItemInfo;
     }
 
     @Override
     public Iterator<MenuItemInfo> iterator() {
-        /*  добавьте реализацию*/
-        return null;
+        return new Iterator<>() {
+            private DFSIterator dfsIterator = new DFSIterator();
+
+            @Override
+            public boolean hasNext() {
+                return dfsIterator.hasNext();
+            }
+
+            @Override
+            public MenuItemInfo next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return select(dfsIterator.next().menuItem.getName()).get();
+            }
+        };
     }
 
     private Optional<ItemInfo> findItem(String name) {
-        /*  добавьте реализацию*/
-        return null;
+        Optional<ItemInfo> itemInfo = Optional.empty();
+        Iterator<ItemInfo> iterator = new DFSIterator();
+        ItemInfo tempItemInfo = null;
+        while (iterator.hasNext()) {
+            tempItemInfo = iterator.next();
+            if (name.equals(tempItemInfo.menuItem.getName())) {
+                itemInfo = Optional.of(tempItemInfo);
+                break;
+            }
+        }
+        return itemInfo;
     }
 
     private static class SimpleMenuItem implements MenuItem {
@@ -58,9 +100,9 @@ public class SimpleMenu implements Menu {
 
     private class DFSIterator implements Iterator<ItemInfo> {
 
-        Deque<MenuItem> stack = new LinkedList<>();
+        private Deque<MenuItem> stack = new LinkedList<>();
 
-        Deque<String> numbers = new LinkedList<>();
+        private Deque<String> numbers = new LinkedList<>();
 
         DFSIterator() {
             int number = 1;
@@ -84,7 +126,7 @@ public class SimpleMenu implements Menu {
             String lastNumber = numbers.removeFirst();
             List<MenuItem> children = current.getChildren();
             int currentNumber = children.size();
-            for (var i = children.listIterator(children.size()); i.hasPrevious();) {
+            for (var i = children.listIterator(children.size()); i.hasPrevious(); ) {
                 stack.addFirst(i.previous());
                 numbers.addFirst(lastNumber.concat(String.valueOf(currentNumber--)).concat("."));
             }
@@ -94,8 +136,8 @@ public class SimpleMenu implements Menu {
 
     private class ItemInfo {
 
-        MenuItem menuItem;
-        String number;
+        private MenuItem menuItem;
+        private String number;
 
         public ItemInfo(MenuItem menuItem, String number) {
             this.menuItem = menuItem;
