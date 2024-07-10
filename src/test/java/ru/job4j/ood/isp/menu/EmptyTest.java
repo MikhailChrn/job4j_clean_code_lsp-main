da5db954e5507091591c15316bb33cf5e4fa4286
MikhailChrn <mikhail.cherneyev@yandex.ru>
2024-07-10 22:16:33 +0400

    Add empty test.
    Повторно.

---
+++ src/test/java/ru/job4j/ood/isp/menu/EmptyTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.ood.isp.menu;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Тестирование работы git.
+ */
+
+public class EmptyTest {
+    @Test
+    public void whenEmptyTest() {
+
+    }
+}
