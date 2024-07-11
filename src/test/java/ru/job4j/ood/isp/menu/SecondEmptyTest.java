badb1e452479b5811d9eb3343fd25b1c9fefa8cb
MikhailChrn <mikhail.cherneyev@yandex.ru>
2024-07-11 23:32:41 +0400

    Add SecondEmptyTest
    add first void
    
    add comment
    
    and one more comment

---
+++ src/test/java/ru/job4j/ood/isp/menu/SecondEmptyTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.ood.isp.menu;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Добавлен комментарий.
+ * Добавлен ещё комментарий.
+ * Добавлен ещё один комментарий.
+ */
+
+public class SecondEmptyTest {
+    @Test
+    public void whenEmptyTest() {
+
+    }
+}
