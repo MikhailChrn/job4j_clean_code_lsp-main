673625903f0251933e0152686b7a42ff5548e3a5
MikhailChrn <mikhail.cherneyev@yandex.ru>
2024-07-13 23:48:33 +0400

    Commit merge from 4 to 1.

---
+++ src/test/java/ru/job4j/ood/isp/menu/ThirdEmptyTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.ood.isp.menu;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Добавлен первый комментарий.
+ * Добавлен второй комментарий.
+ * Добавлен третий комментарий.
+ */
+
+public class ThirdEmptyTest {
+    @Test
+    public void whenEmptyTest() {
+
+    }
+}
