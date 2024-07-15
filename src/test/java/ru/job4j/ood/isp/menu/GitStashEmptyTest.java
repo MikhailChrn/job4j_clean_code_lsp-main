74bf67100d30fb92afc1096a9ed7e7149a946b4f
MikhailChrn <mikhail.cherneyev@yandex.ru>
2024-07-15 21:54:06 +0400

    Add comments into empty test.
    Add comments into empty test.

--- src/test/java/ru/job4j/ood/isp/menu/GitStashEmptyTest.java
+++ src/test/java/ru/job4j/ood/isp/menu/GitStashEmptyTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 
 public class GitStashEmptyTest {
     @Test
-    void whenSecondEmptyTest(){
+    void whenSecondEmptyTest() {
         
+    }
+
+    @Test
+    void whenFirstEmptyTest() {
+
     }
 }
